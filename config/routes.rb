99319f969cddbcebff1f7021156d0959f2d8d2fa
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-05-19 02:01:18 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'singup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
